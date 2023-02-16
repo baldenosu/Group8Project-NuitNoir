50b6542bb7262e7b83a389d4289fc0c7f6de6b46
chavezdo <122295223+chavezdo@users.noreply.github.com>
2023-02-16 12:50:57 -0800

    Browse Feature Enabled
    For app.js, customers.hbs and employees.hbs. Also added public folder.

--- app.js
+++ app.js
@@ -8,18 +8,14 @@ var app = express();
 PORT = 9037;
 const path = require('path');
 // Database
-// var db = require('./database/db-connector');
+var db = require('./database/db-connector');
 
 // Handlebars
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
-
-
 app.set('views', path.join(__dirname, 'views'));
-
-
 /*
     ROUTES
 */
@@ -30,12 +26,18 @@ app.get('/', function(req, res)
 
 app.get('/customers', function(req, res)
     {
-        res.render('customers');
+        let query1 = "SELECT * FROM Customers;";
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('customers', {data: rows});
+        })
     });
 
 app.get('/employees', function(req, res)
     {
-        res.render('employees');
+        let query2 = "SELECT * FROM Employees;";
+        db.pool.query(query2, function(error, rows, fields){
+            res.render('employees', {data: rows});
+        })
     });
 
 app.get('/orders', function(req, res)
